f9178ab0de85578a1ce9563f470faab9dadc7321
Pascal Fischert <p-fischer@gmx.ne>
2023-07-10 14:08:45 +0200

    add spryker/glossary to dependencies

--- src/FondOfSpryker/Zed/Oms/Communication/OmsCommunicationFactory.php
+++ src/FondOfSpryker/Zed/Oms/Communication/OmsCommunicationFactory.php
@@ -3,7 +3,7 @@
 namespace FondOfSpryker\Zed\Oms\Communication;
 
 use FondOfSpryker\Zed\Oms\Dependency\Facade\OmsToGlossaryFacadeInterface;
-use Pyz\Zed\Oms\OmsDependencyProvider;
+use FondOfSpryker\Zed\Oms\OmsDependencyProvider;
 use Spryker\Zed\Oms\Communication\OmsCommunicationFactory as SprykerOmsCommunicationFactory;
 
 /**
